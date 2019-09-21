bc7b02c2db06ceb63f9c735e9fc716d6363031e2
Zirui <fanzirui@yahoo.com>
2019-09-21 20:34:33 +0800

    修改了警告形式

--- docs/script.js
+++ docs/script.js
@@ -1,11 +1,8 @@
-var warning = document.querySelector("#warning");
 var line = document.querySelector(".line");
 var copyright = document.querySelector("#copyright");
 
-function closeWarning() {
-    warning.style.height = "0";
-    warning.style.margin = "0";
-    warning.style.visibility = "hidden";
+function warn() {
+    alert("大部分周记分布在第三方图文创作分享网站（如美篇）各位同学的账号下，删除将导致无法访问，因此如须删除请提前联系制作人员，将该篇文章转移。");
 }
 
 function adjustLine() {
