cf6f701c5bb2e65b0a9b0d4fdaa5587520f3d7f5
Zirui <fanzirui@yahoo.com>
2019-09-26 08:35:49 +0800

    bug fixes

--- docs/script.js
+++ docs/script.js
@@ -1,14 +1,9 @@
 var line = document.querySelector(".line");
+var timeline = document.querySelector("ul");
 var copyright = document.querySelector("#copyright");
 
 function warn() {
     alert("大部分周记分布在第三方图文创作分享网站（如美篇）各位同学的账号下，删除将导致无法访问，因此如须删除请提前联系制作人员，将该篇文章转移。");
-}
-
-function adjustLine() {
-    var height = document.body.clientHeight;
-
-    line.style.height = height - 750 + "px";
 }
 
 function insertYear() {
@@ -19,7 +14,5 @@ function insertYear() {
 }
 
 function init() {
-    adjustLine();
-
     insertYear();
 }
