e8d6258ef5d1a12beffb90fe23cccead76163520
Zirui <fanzirui@yahoo.com>
2019-10-03 14:45:11 +0800

    Update

--- script.js
+++ script.js
@@ -1,9 +1,8 @@
-var line = document.querySelector(".line");
-var copyright = document.querySelector("#copyright");
-
 function warn() {
     alert("大部分周记分布在第三方图文创作分享网站（如美篇）各位同学的账号下，删除将导致无法访问，因此如须删除请提前联系制作人员，将该篇文章转移。");
 }
+
+var copyright = document.querySelector("#copyright");
 
 function insertYear() {
     var date = new Date;
@@ -17,6 +16,20 @@ function insertYear() {
     }
 }
 
+var line = document.querySelector(".line");
+var startDot = document.querySelector("#start");
+var endDot = document.querySelector("#end");
+
+function adjustLine() {
+    var startY = startDot.offsetTop;
+    var endY = endDot.offsetTop;
+    var height = endY - startY;
+
+    line.style.height = height + "px";
+}
+
 window.onload = function() {
     insertYear();
+
+    adjustLine();
 }
